26b33e4746460795d080f91029e04aa7cedbe2e1
Zaphod Beeblebrox <brox42@gmail.com>
2024-07-09 22:46:56 +0300

    root fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import preact from '@preact/preset-vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  // base: "/icfpc2024_3d/",
+  base: "/icfpc2024_3d/",
 })
